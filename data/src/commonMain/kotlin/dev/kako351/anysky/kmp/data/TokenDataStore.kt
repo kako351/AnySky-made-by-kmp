cb424857291c2c03a56325727cb95104a890b1de
kako351 <h3.birth@gmail.com>
2024-09-16 01:23:26 +0900

    create token datastore

---
+++ data/src/commonMain/kotlin/dev/kako351/anysky/kmp/data/TokenDataStore.kt
@@ -0,0 +1,12 @@
+package dev.kako351.anysky.kmp.data
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import okio.Path.Companion.toPath
+
+fun createDataStore(producePath: () -> String): DataStore<Preferences> = PreferenceDataStoreFactory.createWithPath(
+    produceFile = { producePath().toPath() }
+)
+
+internal const val dataStoreFileName = "any_sky_token.preferences_pb"
---
+++ data/src/iosMain/kotlin/dev/kako351/anysky/kmp/data/TokenDataStore.ios.kt
@@ -0,0 +1,13 @@
+
+fun createDataStore(): DataStore<Preferences> = createDataStore(
+    producePath = {
+        val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null,
+        )
+        requireNotNull(documentDirectory).path + "/$dataStoreFileName"
+    }
+)
