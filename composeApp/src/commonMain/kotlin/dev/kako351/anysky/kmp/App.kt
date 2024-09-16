dc8f12a34d2708d52a2fab32ac70746d1b4b2b8b
kako351 <h3.birth@gmail.com>
2024-09-16 20:16:47 +0900

    delete no used code

--- composeApp/src/commonMain/kotlin/dev/kako351/anysky/kmp/App.kt
+++ composeApp/src/commonMain/kotlin/dev/kako351/anysky/kmp/App.kt
@@ -1,15 +1,8 @@
 package dev.kako351.anysky.kmp
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -17,17 +10,13 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import anysky_made_by_kmp.composeapp.generated.resources.Res
-import anysky_made_by_kmp.composeapp.generated.resources.compose_multiplatform
-import kotlinx.coroutines.flow.MutableStateFlow
 import dev.kako351.anysky.kmp.data.TokenDataStore
 import dev.kako351.anysky.kmp.data.model.auth.AccessToken
 import dev.kako351.anysky.kmp.ui.FeedsScreen
 import dev.kako351.anysky.kmp.ui.LoginScreen
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import org.koin.compose.viewmodel.koinViewModel
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 enum class AppScreen {
     Login,
